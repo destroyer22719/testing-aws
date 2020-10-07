043bd18601a8514cf518d4292543b84b9db4ae7a
Nathan <nathancai1031@gmail.com>
2020-10-07 15:21:44 -0400

    testing deployment

--- app.js
+++ app.js
@@ -4,11 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('', (req, res) => {
-  res.send('hello world')
-})
-
-app.get('/env', (req, res) => {
-  res.send(`${process.env.KEY}`)
+  res.send('changed')
 })
 
 app.listen(port, () => {
