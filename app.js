e10457a987ae225ec9548218f248eee2aaf79747
Nathan <nathancai1031@gmail.com>
2020-10-07 16:43:42 -0400

    please jesus son of god work

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('', (req, res) => {
-  res.send('version should work with tag')
+  res.send('OMG PLEASE WORK T_T')
 })
 
 app.listen(port, () => {
