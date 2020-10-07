a5a8216ab98b69205078940cd30f705e1c91d07c
Nathan <nathancai1031@gmail.com>
2020-10-07 16:36:18 -0400

    testing

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('', (req, res) => {
-  res.send('deployed from elastic deploy')
+  res.send('version should work with tag')
 })
 
 app.listen(port, () => {
